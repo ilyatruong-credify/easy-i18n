0e74c15ec0f640ad7243f6e250e1b4568aaf4db8
hungphongbk <programmingd32@gmail.com>
2021-04-11 01:51:31 +0700

    view entry type (text, contains HTML or variable)

--- src/main/kotlin/com/yuukaze/i18next/ui/renderer/CustomTableCellRenderer.kt
+++ src/main/kotlin/com/yuukaze/i18next/ui/renderer/CustomTableCellRenderer.kt
@@ -1,9 +1,18 @@
 package com.yuukaze.i18next.ui.renderer
 
+import com.intellij.icons.AllIcons
 import com.intellij.ui.JBColor
 import java.awt.Component
+import javax.swing.Icon
+import javax.swing.JLabel
 import javax.swing.JTable
 import javax.swing.table.DefaultTableCellRenderer
+
+private object DetectRegexes {
+  val variable = Regex("\\{\\{[a-z]+}}")
+  val containsHTML =
+    Regex("</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[^'\">\\s]+))?)+\\s*|\\s*)/?>");
+}
 
 /**
  * Similar to [DefaultTableCellRenderer] but will mark the first column red if any column is empty.
@@ -18,7 +27,23 @@ class CustomTableCellRenderer : DefaultTableCellRenderer() {
     row: Int,
     column: Int
   ): Component {
-    val component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+    val component = super.getTableCellRendererComponent(
+      table,
+      value,
+      isSelected,
+      hasFocus,
+      row,
+      column
+    ) as JLabel
+    if (column == 0) {
+      val icon = ListIcon(
+        listOf(
+          detectValuableEntry(row, table!!),
+          detectContainsHTMLEntry(row, table!!)
+        )
+      )
+      component.icon = icon
+    } else component.icon = null
     if (column == 0 && missesValues(row, table!!)) {
       component.foreground = JBColor.RED
     } else { // Reset color
@@ -36,5 +61,15 @@ class CustomTableCellRenderer : DefaultTableCellRenderer() {
       }
     }
     return false
+  }
+
+  private fun detectValuableEntry(row: Int, table: JTable): Icon {
+    val value = table.getValueAt(row, 1) as String
+    return if (DetectRegexes.variable.containsMatchIn(value)) AllIcons.Nodes.Variable else AllIcons.Nodes.EmptyNode
+  }
+
+  private fun detectContainsHTMLEntry(row: Int, table: JTable): Icon {
+    val value = table.getValueAt(row, 1) as String
+    return if (DetectRegexes.containsHTML.containsMatchIn(value)) AllIcons.Xml.Html_id else AllIcons.Nodes.EmptyNode
   }
 }
